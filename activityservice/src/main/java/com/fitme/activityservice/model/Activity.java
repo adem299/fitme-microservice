7abd25fa1b80274df8f48ab3bb5b55e735155293
adem299 <mulyanaade30@gmail.com>
2026-08-19 07:37:42 +0700

    feat: initialize activity service with configuration files and core application structure

---
+++ activityservice/src/main/java/com/fitme/activityservice/model/Activity.java
@@ -0,0 +1,34 @@
+package com.fitme.activityservice.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.Data;
+
+@Document(collection = "activities")
+@Data
+
+public class Activity {
+    @Id
+    private String id;
+    private String userId;
+    private ActivityType activityType;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+
+    @Field("metrics")
+    private Map<String, Object> additionalMetrics;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
---
+++ activityservice/src/main/java/com/fitme/activityservice/model/ActivityType.java
@@ -0,0 +1,5 @@
+package com.fitme.activityservice.model;
+
+public enum ActivityType {
+    RUNNING, WALKING, CYCLING, SWIMMING, WEIGHTLIFTING, YOGA, CARDIO, STRETCHING
+}
